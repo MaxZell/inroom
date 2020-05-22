94cdf4d74b687a2bee06da12d30087f19d286c7e
mzelensky <max.m.zelensky@gmail.com>
2020-05-22 14:17:25 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿window.onload = () => {
-    // const button = document.querySelector('button[data-action="change"]');
-    // button.innerText = '﹖';
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '﹖';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
